e6fa8805e79215323cc6fd3dfc74d606910eadf5
WRLCA\sanming <sanming@lightchaseranimation.com>
2018-04-15 11:04:29 +0800

    头文件问题

--- delaunayTest.cpp
+++ delaunayTest.cpp
@@ -1,6 +1,8 @@
 ﻿#include <vector>
 #include <algorithm>
 #include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 
 using namespace cv;
 using namespace std;
